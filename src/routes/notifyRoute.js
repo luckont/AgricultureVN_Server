6b2f5b002f47df0fcbc78429e3df5c8e9596dad6
luckont <luckont4545@gmail.com>
2023-10-08 23:51:38 +0700

    Delete all notify

--- src/routes/notifyRoute.js
+++ src/routes/notifyRoute.js
@@ -1,11 +1,15 @@
-const jwtMiddleware = require("../middlewares/jwtMiddleware")
+const jwtMiddleware = require("../middlewares/jwtMiddleware");
 const router = require("express").Router();
 const notifyController = require("../controllers/notifyController");
 
-router.post("/", jwtMiddleware.verifyToken, notifyController.createNotify)
+router.post("/", jwtMiddleware.verifyToken, notifyController.createNotify);
 
-router.delete("/:id", jwtMiddleware.verifyToken, notifyController.removeNotify)
+router.delete("/:id", jwtMiddleware.verifyToken, notifyController.removeNotify);
 
-router.get("/", jwtMiddleware.verifyToken, notifyController.getNotifies)
+router.get("/", jwtMiddleware.verifyToken, notifyController.getNotifies);
 
-module.exports = router
+router.put("/isReadNotify/:id", jwtMiddleware.verifyToken, notifyController.isReadNotify);
+
+router.delete("/", jwtMiddleware.verifyToken, notifyController.deleteAllNotifies);
+
+module.exports = router;
